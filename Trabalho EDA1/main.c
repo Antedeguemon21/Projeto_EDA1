eac887757c2bfa9e6c629a5effc35d97ae4477e4
Arthur <165080772+ArthurziEvan@users.noreply.github.com>
2026-01-28 22:39:17 -0300

    Adicionando Diretório com a main implementada

---
+++ Trabalho EDA1/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Cliente {
+    char cpf[15];
+    char nome[100];
+    char email[100];
+    char telefone[20];
+    char nascimento[11];
+    struct Cliente *prox;
+} Cliente;
+
+typedef struct Produto {
+    int codigo;
+    char nome[100];
+    float preco;
+    int quantidade;
+    struct Produto *prox;
+} Produto;
+
+typedef struct ItemCarrinho {
+    int codigoProduto;
+    int quantidade;
+    struct ItemCarrinho *prox;
+} ItemCarrinho;
+
+typedef struct Carrinho {
+    char cpf_cliente[15];
+    ItemCarrinho *itens;
+    struct Carrinho *prox;
+} Carrinho;
+
+int main () {
+
+    return 0;
+}

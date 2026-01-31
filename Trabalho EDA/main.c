5e151605116c9e6c242c9b815a331bc89727e02d
Antedeguemon21 <leonardo.2003rmartins@gmail.com>
2026-01-31 12:17:34 -0300

    Update main.c

--- Trabalho EDA/main.c
+++ Trabalho EDA/main.c
@@ -1,2 +1,13 @@
-Cliente *listaClientes = NULL;
-//Prototipo de main. Iniciando a lista, ainda vazia.
+int main() {
+    Cliente *listaClientes = NULL;
+
+    cadastrarCliente(&listaClientes);
+    cadastrarCliente(&listaClientes);
+    listarClientes(listaClientes);
+
+    removerCliente(&listaClientes);
+    listarClientes(listaClientes);
+
+    return 0;
+}
+// Apenas para testes. Excluir depois

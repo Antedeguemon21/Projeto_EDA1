5e5445e3652d03bf1ef36314a0669a0d4723d8a6
Arthur Evangelista da Silva <165080772+ArthurziEvan@users.noreply.github.com>
2026-02-01 13:05:32 -0300

    atualizando crud  do carrinho de compras

--- Trabalho EDA/Carrinho/carrinhoo.c
+++ Trabalho EDA/Carrinho/carrinhoo.c
@@ -19,3 +19,62 @@ Carrinho* buscarCarrinho(Carrinho *lista, char cpf[]) {
     }
     return NULL;
 }
+
+
+void adicionarProdutoCarrinho(Carrinho *c, Produto *produtos, int codigo, int qtd) {
+    if (!c) return;
+
+    Produto *p = buscarProduto(produtos, codigo);
+    if (!p) {
+        printf("Produto não encontrado.\n");
+        return;
+    }
+
+    
+    if (p->quantidade < qtd) {
+        printf("Quantidade insuficiente no estoque\n");
+        return;
+    }
+
+    ItemCarrinho *novo = malloc(sizeof(ItemCarrinho));
+    if (!novo) return;
+
+    novo->codigo = codigo;
+    novo->quantidade = qtd;
+    novo->prox = c->itens;
+    c->itens = novo;
+
+    p->quantidade -= qtd;
+
+    printf("Produto adicionado.\n");
+}
+
+void removerProdutoCarrinho(Carrinho *c, Produto *produtos, int codigo) {
+    if (!c || !c->itens) return;
+
+    ItemCarrinho *ant = NULL;
+    ItemCarrinho *atual = c->itens;
+
+    while (atual && atual->codigo != codigo) {
+        ant = atual;
+        atual = atual->prox;
+    }
+
+    if (!atual) {
+        printf("Produto não encontrado no carrinho.\n");
+        return;
+    }
+
+    Produto *p = buscarProduto(produtos, codigo);
+    if (p) {
+        p->quantidade += atual->quantidade;
+    }
+
+    if (ant)
+        ant->prox = atual->prox;
+    else
+        c->itens = atual->prox;
+
+    free(atual);
+    printf("Produto removido do carrinho.\n");
+}

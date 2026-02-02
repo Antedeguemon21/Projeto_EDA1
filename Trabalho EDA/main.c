0a3075dfcfb079c11bef07a23df43fa0d1518c2f
Arthur Evangelista da Silva <165080772+ArthurziEvan@users.noreply.github.com>
2026-02-02 19:22:06 -0300

    Finalizando a main

--- Trabalho EDA/main.c
+++ Trabalho EDA/main.c
@@ -11,16 +11,15 @@ void menuPrincipal() {
     printf("2 - Gerenciar Produtos\n");
     printf("3 - Modo Compra\n");
     printf("0 - Sair\n");
-    printf("Opcao: ");
+    printf("Opcao ");
 }
-
 void menuClientes() {
     printf("\n-=-=-=- MENU CLIENTES =-=-=-=-=\n");
     printf("1 - Cadastrar\n");
     printf("2 - Listar\n");
     printf("3 - Editar\n");
     printf("4 - Remover\n");
-    printf("Opcao: ");
+    printf("Opcao ");
 }
 
 void menuProdutos() {
@@ -29,7 +28,7 @@ void menuProdutos() {
     printf("2 - Listar\n");
     printf("3 - Editar\n");
     printf("4 - Remover\n");
-    printf("Opcao: ");
+    printf("Opcao ");
 }
 
 void menuCarrinho() {
@@ -37,9 +36,85 @@ void menuCarrinho() {
     printf("1 - Adicionar produto\n");
     printf("2 - Remover produto\n");
     printf("3 - Listar carrinho\n");
-    printf("Opcao: ");
+    printf("Opcao ");
 }
+int main() {
+    Cliente *clientes = NULL;
+    Produto *produtos = NULL;
+    Carrinho *carrinhos = NULL;
 
-int main () {
-    
+    int op, sub, codigo, qtd;
+    char cpf[15];
+    do {
+        menuPrincipal();
+        scanf("%d", &op);
+
+        switch (op) {
+
+        case 1:
+            menuClientes();
+            scanf("%d", &sub);
+
+            if (sub == 1)
+                cadastrarCliente(&clientes);
+            else if (sub == 2)
+                listarClientes(clientes);
+            else if (sub == 3)
+                editarCliente(clientes);
+            else if (sub == 4)
+                removerCliente(&clientes);
+            break;
+
+        case 2:
+            menuProdutos();
+            scanf("%d", &sub);
+
+            if (sub == 1)
+                produtos = cadastrarProduto(produtos);
+            else if (sub == 2)
+                listarProdutos(produtos);
+            else if (sub == 3) {
+                printf("Codigo do produto: ");
+                scanf("%d", &codigo);
+                produtos = editarProduto(produtos, codigo);
+            }
+            else if (sub == 4) {
+                printf("Codigo do produto: ");
+                scanf("%d", &codigo);
+                produtos = removerProduto(produtos, codigo);
+            }
+            break;
+
+        case 3:
+            printf("CPF do cliente: ");
+            scanf("%s", cpf);
+
+            Carrinho *c = buscarCarrinho(carrinhos, cpf);
+            if (!c)
+                carrinhos = criarCarrinho(carrinhos, cpf);
+
+            c = buscarCarrinho(carrinhos, cpf);
+
+            menuCarrinho();
+            scanf("%d", &sub);
+
+            if (sub == 1) {
+                printf("Codigo do produto: ");
+                scanf("%d", &codigo);
+                printf("Quantidade: ");
+                scanf("%d", &qtd);
+                adicionarProdutoCarrinho(c, produtos, codigo, qtd);
+            }
+            else if (sub == 2) {
+                printf("Codigo do produto: ");
+                scanf("%d", &codigo);
+                removerProdutoCarrinho(c, produtos, codigo);
+            }
+            else if (sub == 3)
+                listarCarrinho(c, produtos);
+            break;
+        }
+    } while (op != 0);
+    printf("\nPrograma encerrado.\n");
+    return 0;
 }

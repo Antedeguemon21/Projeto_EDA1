cf6844c662470a2ac986f05e135763b5a7cf6d15
Arthur Evangelista da Silva <165080772+ArthurziEvan@users.noreply.github.com>
2026-02-02 12:49:10 -0300

    Adicionando menu inicial

--- Trabalho EDA/main.c
+++ Trabalho EDA/main.c
@@ -1,13 +1,45 @@
-int main() {
-    Cliente *listaClientes = NULL;
+#include <stdio.h>
+#include "Clientees.h"
+#include "produto.h"
+#include "carrinho.h"
 
-    cadastrarCliente(&listaClientes);
-    cadastrarCliente(&listaClientes);
-    listarClientes(listaClientes);
-
-    removerCliente(&listaClientes);
-    listarClientes(listaClientes);
-
-    return 0;
+void menuPrincipal() {
+    printf("\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("      SISTEMA DE VENDAS      \n");
+    printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n");
+    printf("1 - Gerenciar Clientes\n");
+    printf("2 - Gerenciar Produtos\n");
+    printf("3 - Modo Compra\n");
+    printf("0 - Sair\n");
+    printf("Opcao: ");
 }
-// Apenas para testes. Excluir depois
+
+void menuClientes() {
+    printf("\n-=-=-=- MENU CLIENTES =-=-=-=-=\n");
+    printf("1 - Cadastrar\n");
+    printf("2 - Listar\n");
+    printf("3 - Editar\n");
+    printf("4 - Remover\n");
+    printf("Opcao: ");
+}
+
+void menuProdutos() {
+    printf("\n-=-=-= MENU PRODUTOS =-=-=\n");
+    printf("1 - Cadastrar\n");
+    printf("2 - Listar\n");
+    printf("3 - Editar\n");
+    printf("4 - Remover\n");
+    printf("Opcao: ");
+}
+
+void menuCarrinho() {
+    printf("\n=-=-=-=- MENU CARRINHO =-=-=-=\n");
+    printf("1 - Adicionar produto\n");
+    printf("2 - Remover produto\n");
+    printf("3 - Listar carrinho\n");
+    printf("Opcao: ");
+}
+
+int main () {
+    
+}

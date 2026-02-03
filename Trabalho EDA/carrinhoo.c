5a36a16a2ac6a7cd8c3a8479c04579e7cf453ab3
Arthur Evangelista da Silva <165080772+ArthurziEvan@users.noreply.github.com>
2026-02-03 12:38:19 -0300

    Carrinhos finalizados!

--- Trabalho EDA/carrinhoo.c
+++ Trabalho EDA/carrinhoo.c
@@ -5,7 +5,7 @@
 
     Carrinho* criarCarrinho(Carrinho *lista, char cpf[]) {
         Carrinho *novo = malloc(sizeof(Carrinho));
-        strcpy(novo->cpf, cpf);
+        strcpy(novo->cpf_cliente, cpf);
         novo->itens = NULL;
         novo->prox = lista;
         return novo; }
@@ -13,7 +13,7 @@
 
     Carrinho* buscarCarrinho(Carrinho *lista, char cpf[]) {
         while (lista) {
-            if (strcmp(lista->cpf, cpf) == 0)
+            if (strcmp(lista->cpf_cliente, cpf) == 0)
                 return lista;
             lista = lista->prox;
         }
@@ -39,7 +39,7 @@
         ItemCarrinho *novo = malloc(sizeof(ItemCarrinho));
         if (!novo) return;
 
-        novo->codigo = codigo;
+        novo->codigoProduto = codigo;
         novo->quantidade = qtd;
         novo->prox = c->itens;
         c->itens = novo;
@@ -55,7 +55,7 @@
         ItemCarrinho *ant = NULL;
         ItemCarrinho *atual = c->itens;
 
-        while (atual && atual->codigo != codigo) {
+        while (atual && atual->codigoProduto != codigo) {
             ant = atual;
             atual = atual->prox;
         }
@@ -89,10 +89,10 @@
     int totalItens = 0;
     float valorTotal = 0;
 
-    printf("Carrinho do cliente CPF %s:\n", c->cpf);
+    printf("Carrinho do cliente CPF %s:\n", c->cpf_cliente);
 
     while (it) {
-        Produto *p = buscarProduto(produtos, it->codigo);
+        Produto *p = buscarProduto(produtos, it->codigoProduto);
         if (p) {
             printf("Codigo: %d | Nome  %s | Quantidade  %d | Subtotal  %.2f\n",
                    p->codigo, p->nome, it->quantidade, p->preco * it->quantidade);
